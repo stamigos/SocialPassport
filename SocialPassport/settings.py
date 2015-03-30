47795ec3716117058eb0815fa342c4bbbecf544f
stamigos <oldtigersvoice@gmail.com>
2015-03-30 15:52:03 +0300

    changed db from sqlite3 to mysql

--- SocialPassport/settings.py
+++ SocialPassport/settings.py
@@ -59,9 +59,17 @@ WSGI_APPLICATION = 'SocialPassport.wsgi.application'
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'socialpassport',
+        'USER': 'spuser',
+        'PASSWORD': 'kafedra44',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
