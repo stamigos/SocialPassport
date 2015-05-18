60a9377e45c1e64b52f3a3ec9365438786d8f998
stamigos <oldtigersvoice@gmail.com>
2015-05-18 14:25:42 +0300

    wsgi marks

--- SocialPassport/wsgi.py
+++ SocialPassport/wsgi.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = "SocialPassport.settings"
+import sys
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SocialPassport.settings")
+
+    from django.core.management import execute_from_command_line
+
+    execute_from_command_line(sys.argv)
